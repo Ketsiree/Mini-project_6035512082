2e08c314a2d588ab8fc384751d07c9b8c4968888
Ketsiree <funny24073011@gmail.com>
2020-04-06 19:21:02 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Firebase.
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+// Styles
+import './App.css'; // This uses CSS modules.
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+// Instantiate a Firebase app.
+firebase.initializeApp(firebaseConfig);
+
+class SignInScreen extends Component {
+
+
+  render() {
+    return (
+      <div className="container">
+        <h1>FirebaseUI-React</h1>
+        <h1> with Firebase Authentication</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default SignInScreen;

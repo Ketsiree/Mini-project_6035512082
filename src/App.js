9864fe9db8d15f4b31b58635c5fc6431c0e289b3
Ketsiree <54532915+Ketsiree@users.noreply.github.com>
2020-04-07 22:54:00 +0700

    login

--- src/App.js
+++ src/App.js
@@ -23,19 +23,66 @@ const firebaseConfig = {
 // Instantiate a Firebase app.
 firebase.initializeApp(firebaseConfig);
 
+
 class SignInScreen extends Component {
 
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google , Facebook , Etc as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccess: () => false
+    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
 
   render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div className="container">
+          <h1>FirebaseUI-React</h1>
+          <h1> with Firebase Authentication</h1>
+
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      );
+    }
     return (
       <div className="container">
         <h1>FirebaseUI-React</h1>
         <h1> with Firebase Authentication</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+          <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL}/>
+        <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
       </div>
     );
   }
 }
 
 export default SignInScreen;
-
-

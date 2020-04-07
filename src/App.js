e359b54a6121804bbfdfab6e5534825bb747d063
Ketsiree <54532915+Ketsiree@users.noreply.github.com>
2020-04-07 21:02:18 +0700

    123

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import './App.css'; // This uses CSS modules.
 
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCEuHstUes2E1COX2kueelZWQ8XZwxRjVQ",
+  authDomain: "mini-project-6035512082.firebaseapp.com",
+  databaseURL: "https://mini-project-6035512082.firebaseio.com",
+  projectId: "mini-project-6035512082",
+  storageBucket: "mini-project-6035512082.appspot.com",
+  messagingSenderId: "1079564372345",
+  appId: "1:1079564372345:web:3bae5f48d098ce16ad47a8",
+  measurementId: "G-EREX068PDC"
+};
 // Instantiate a Firebase app.
 firebase.initializeApp(firebaseConfig);
 
@@ -35,3 +37,5 @@ class SignInScreen extends Component {
 }
 
 export default SignInScreen;
+
+
